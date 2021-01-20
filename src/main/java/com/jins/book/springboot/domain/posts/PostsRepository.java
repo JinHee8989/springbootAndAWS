db6cac09d6a78bd74e64dc8aa21c2af2a9f4c472
wlsgm <jinheebyeon89@gmail.com>
2021-01-20 17:50:00 +0900

    #게시글 수정, 삭제 기능 추가

--- src/main/java/com/jins/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jins/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,14 @@
 package com.jins.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Arrays;
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> { //JpsRepository<Entity클래스, PK타입>하면 기본적인 CRUD 메소드가 자동으로 생성됨
+
+    @Query(value = "SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
 }
