a6677f3185d030a638317604fb53a4331d740de6
jinheebyeon <wlsgml8989@gmail.com>
2021-01-23 23:06:30 +0900

    # 커밋테스트

--- src/main/java/com/jins/book/springboot/web/HelloController.java
+++ src/main/java/com/jins/book/springboot/web/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //컨트롤러를 json을 반환하는 컨트롤러로 만들어줌
+@RestController //컨트롤러를 json을 반환하는 컨트롤러로 만들어줌 ..
                 // (예전에 @ResponseBody를 각 메소드마다 선언했던걸 한번에 사용할 수 있게 해줌)
 public class HelloController {
 
