3de31ca48e8dde894a700fa5432b7bfcfee319d5
wlsgm <wlsgml8989@gmail.com>
2021-02-20 11:11:40 +0900

    #  코드수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,7 +9,7 @@ source ${ABSDIR}/profile.sh
 function switch_proxy(){
   IDLE_PORT=$(find_idle_port)
 
-  echo ">전환할 Port: $IDLE_PORT"
+  echo "> 전환할 Port: $IDLE_PORT"
   echo ">Port 전환"
   echo ">set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc     # 하나의 문장을 만들어 파이프라인(|)으로 넘겨주기위해 echo를 사용
                                                                                                             # 엔진엑스가 변경할 프록시 주소를 생성
